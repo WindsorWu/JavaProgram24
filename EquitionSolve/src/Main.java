0a27fa763f6b5657c2b62af70e24a2f109302013
WindsorWu <wenzhewu2@outlook.com>
2024-10-10 10:59:52 +0800

    update

---
+++ EquitionSolve/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+            Equition equition = new Equition(1,5,3);
+            if(equition.solve())
+                equition.print();
+    }
+}
