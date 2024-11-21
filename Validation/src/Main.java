4248b214b9981744988a5dffd2dbaf5c828f61ef
WindsorWu <wenzhewu2@outlook.com>
2024-11-21 10:47:57 +0800

    1、Validation update

--- Validation/src/Main.java
+++ Validation/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
+    private static final String EMAIL_REGEX="^[A-Za-z0-9+_.-]+@[A-Za-z0-9+_.-]+$";
+    public static boolean isValidEmail(String email){
+        if(email == null){
+            return false;
+        }
+        Pattern pattern =Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if(isValidEmail(s)){
+            System.out.println("这是合法的邮箱");
+        }
+        else{
+            System.out.println("这不是合法的邮箱");
+        }
     }
 }
